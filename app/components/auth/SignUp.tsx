3415fb8c80121571db1f7dbafb0285c334d4438f
carlosgodspeed <carlosminibics@gmail.com>
2025-03-20 15:23:58 -0300

    tela de login e cadastro criadas

--- app/components/auth/SignUp.tsx
+++ app/components/auth/SignUp.tsx
@@ -1,14 +1,19 @@
-import { View, Text, Image, TextInput, StyleSheet } from 'react-native';
+import { View, Text, Image, TextInput, StyleSheet, TouchableOpacity, Pressable } from 'react-native';
 import React from 'react';
 import Colors from '../constant/Colors';
+import { useRouter } from 'expo-router';
 
 export default function SignUp() {
+
+    const router=useRouter();
+
 return (
     <View style={{
         display:'flex',
         alignItems:'center',
         paddingTop:100,
         flex:1,
+        padding:25,
         backgroundColor:Colors.WHITE,
     }}>
     <Image source={require('./../../assets/images/logo.png')}
@@ -24,12 +29,58 @@ return (
     }}>Create New Account</Text>
 
     <TextInput placeholder='Full Name' style={styles.textInput} />
+    <TextInput placeholder='Email' style={styles.textInput} />
+    <TextInput placeholder='Password' secureTextEntry={true} style={styles.textInput} />
+
+    <TouchableOpacity
+    style={{
+        padding:15,
+        backgroundColor:Colors.PRIMARY,
+        width:'100%',
+        marginTop:25,
+        borderRadius:10,
+    }}
+    >
+        <Text style={{
+        fontFamily:'outfit',
+        fontSize:20,
+        color:Colors.WHITE,
+        textAlign:'center',
+        }}>Create Account</Text>
+        
+        </TouchableOpacity>
+
+        <View style={{
+            backgroundColor:Colors.WHITE,
+            display:'flex',
+            flexDirection:'row',
+            gap:5,
+            marginTop:20,
+        }}>
+        <Text style={{
+            fontFamily:'outfit',
+        }}>Already have Account?</Text>
+        <Pressable
+        onPress={()=> router.push('/components/auth/SignIn')}
+        >
+            <Text style={{
+                color:Colors.PRIMARY,
+                fontFamily:'outfit-bold',
+            }}>Sign In Here</Text>
+        </Pressable>
+        </View>
     </View>
     );
 }
 
 const styles = StyleSheet.create({
     textInput:{
-
+        borderWidth: 1,
+        width:'100%',
+        padding:15,
+        fontSize:18,
+        marginTop:20,
+        borderRadius:8,
+        fontFamily:'outfit-bold',
     }
 })
